f298915fb9196da430052ea7bde52651c34a1af1
Salavat <s.gumerov.work@gmail.com>
2021-11-25 23:55:18 +0500

    update AutoriazationForm.tsx

--- src/components/AutoriazationForm.tsx
+++ src/components/AutoriazationForm.tsx
@@ -1,24 +1,30 @@
+import { join } from "path";
 import React, { useState } from "react";
 import { IAutorizationForm } from "../Interfaces";
 
 interface IUserData {
     email: string,
-    password: string,
+    password: string | RegExpMatchArray,
 }
+
+
 
 export const AutoriazationForm: React.FC = () => {
     const [userData, setUserData] = useState<IUserData>({ email: '', password: '' });
 
-    const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newUserData: IUserData = {
-            email: e.target.name === "userEmail" ? e.target.value : userData.email,
-            password: e.target.name === "userPassword" ? e.target.value : userData.password
-        }
+    // const changeHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // }
 
-        setUserData(prev => {
-            return { ...prev, ...newUserData }
-        })
-        console.log(newUserData, userData)
+    const validate = (email: string, password: string) => {
+        console.log(password.match(/[a-zA-Z0-9_]/g))
+        const validPassword = password.match(/[a-zA-Z0-9_]/g)
+        if (validPassword !== null) {
+            setUserData(prev => {
+                return { ...prev, ...{ email: '', password: validPassword.join('') } }
+            });
+        }
+        console.log(userData)
+
     }
 
     const formHandler = (e: React.FormEvent<IAutorizationForm>) => {
@@ -27,20 +33,16 @@ export const AutoriazationForm: React.FC = () => {
             e.currentTarget.elements.userEmail.value,
             e.currentTarget.elements.userPassword.value
         ]
-        console.log(email, password)
+        validate(email, password)
     }
 
     return (
         <form className='m2' onSubmit={formHandler}>
-            <label htmlFor="userEmail">Введите email
-                <input type="email" name="userEmail" id="userEmail" placeholder="Введите email"
-                    onChange={changeHandler}
-                />
+            <label htmlFor="userEmail">{userData.email ? userData.email : 'Введите email'}
+                <input type="email" name="userEmail" id="userEmail" placeholder="Введите email" />
             </label>
-            <label htmlFor="userPassword">Введите пароль
-                <input type="password" name="userPassword" id="userPassword" placeholder="Введите пароль"
-                    onChange={changeHandler}
-                />
+            <label htmlFor="userPassword">{userData.password ? userData.password : 'Введите пароль'}
+                <input type="password" name="userPassword" id="userPassword" placeholder="Введите пароль" />
             </label>
             <button type="submit" className="btn-form">
                 Зарегистрироваться
