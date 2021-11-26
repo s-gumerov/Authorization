8c9e1b5faffcf2dfadf0d359ca1cbf8d460794de
Salavat <s.gumerov.work@gmail.com>
2021-11-27 00:53:24 +0500

    add to useEffect

--- src/components/AutoriazationForm.tsx
+++ src/components/AutoriazationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IAutorizationForm } from "../Interfaces";
 
 interface IUserData {
@@ -9,21 +9,43 @@ interface IUserData {
 
 
 export const AutoriazationForm: React.FC = () => {
-    const [userData, setUserData] = useState<IUserData>({ email: '', password: '' });
+    const [defaultUserData, setDefaultUserData] = useState<IUserData>({ email: 'email', password: 'password' });
+
+    const [userData, setUserData] = useState<IUserData>(defaultUserData);
 
     // const changeHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
     // }
 
+
+
     const validate = (email: string, password: string) => {
         const validPassword = password.match(/[a-zA-Z0-9_]/g)
-
-        if (validPassword !== null) {
-            setUserData(prev => {
-                return { ...prev, ...{ email: email, password: validPassword.join('') } }
-            });
-        }
-        console.log(userData)
+        if (validPassword !== null) setDefaultUserData({
+            email: email,
+            password: validPassword.join('')
+        });
     }
+
+    // useEffect(() => {
+    //     function handleStatusChange(status) {
+    //         setIsOnline(status.isOnline);
+    //     }
+
+    //     ChatAPI.subscribeToFriendStatus(userData, handleStatusChange);
+    //     return () => {
+    //         ChatAPI.unsubscribeFromFriendStatus(userData, handleStatusChange);
+    //     };
+    // }, [defaultUserData, userData]); // Повторно подписаться, только если userData изменился
+
+
+
+
+    useEffect(() => {
+        const newData = defaultUserData;
+        setUserData(newData)
+        console.log(userData)
+
+    }, [defaultUserData, userData])
 
     const formHandler = (e: React.FormEvent<IAutorizationForm>) => {
         e.preventDefault();
@@ -31,15 +53,20 @@ export const AutoriazationForm: React.FC = () => {
             e.currentTarget.elements.userEmail.value,
             e.currentTarget.elements.userPassword.value
         ]
+        // console.log(email, password)
         validate(email, password)
     }
 
+
+
+
+
     return (
         <form className='m2' onSubmit={formHandler}>
-            <label htmlFor="userEmail">{userData.email ? userData.email : 'Введите email'}
+            <label htmlFor="userEmail">Введите email
                 <input type="email" name="userEmail" id="userEmail" placeholder="Введите email" />
             </label>
-            <label htmlFor="userPassword">{userData.password ? userData.password : 'Введите пароль'}
+            <label htmlFor="userPassword">Введите пароль
                 <input type="password" name="userPassword" id="userPassword" placeholder="Введите пароль" />
             </label>
             <button type="submit" className="btn-form">
